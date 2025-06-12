0aded2bad0538a8a35264140aa22cc23eacc41ed
JereSalo <jeresalo17@gmail.com>
2025-06-12 16:48:47 -0300

    add test case

--- sdk/src/create.rs
+++ sdk/src/create.rs
@@ -9,3 +9,17 @@ pub fn compute_create_address(sender_address: Address, sender_nonce: u64) -> Add
     let keccak_bytes = keccak(encoded).0;
     Address::from_slice(&keccak_bytes[12..])
 }
+
+#[test]
+fn compute_address() {
+    use std::str::FromStr;
+
+    // Example Transaction: https://etherscan.io/tx/0x99b6e68fa690db1df9a969b838fb27e1254c0fc115428b3cc5695ab74ffe3943
+    assert_eq!(
+        Address::from_str("0x552b0c6688fcae5cf0164f27fd129b882a42fa05").unwrap(),
+        compute_create_address(
+            Address::from_str("0x899c284A89E113056a72dC9ade5b60E80DD3c94f").unwrap(),
+            1
+        )
+    );
+}
