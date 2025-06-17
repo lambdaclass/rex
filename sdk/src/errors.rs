d793d629eab74b5708ce3f4d78b8b8253c0beae6
Lucas Rack <lucas.rack@lambdaclass.com>
2025-06-17 18:05:16 -0300

    Fix error msgs

--- sdk/src/errors.rs
+++ sdk/src/errors.rs
@@ -10,9 +10,9 @@ pub enum Error {
 pub enum KeystoreError {
     #[error("Error creating default dir: {0}")]
     ErrorCreatingDefaultDir(String),
-    #[error("Error creating Keystore: {0}")]
+    #[error("Error creating keystore: {0}")]
     ErrorCreatingKeystore(String),
-    #[error("Error creating SecretKey: {0}")]
+    #[error("Error creating secret key: {0}")]
     ErrorCreatingSecretKey(String),
     #[error("Error opnening keystore: {0}")]
     ErrorOpeningKeystore(String),
