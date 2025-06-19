dae715de7dc73e9a1ac5d3e5bec2eb176e90c050
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-06-19 11:34:07 -0300

    fix

--- sdk/src/client/eth/mod.rs
+++ sdk/src/client/eth/mod.rs
@@ -110,7 +110,7 @@ impl EthClient {
         tx: &EIP1559Transaction,
         private_key: &SecretKey,
     ) -> Result<H256, EthClientError> {
-        let signed_tx = tx.sign(private_key);
+        let signed_tx = tx.sign(private_key)?;
 
         let mut encoded_tx = signed_tx.encode_to_vec();
         encoded_tx.insert(0, TxType::EIP1559.into());
