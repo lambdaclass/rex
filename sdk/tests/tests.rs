e1ba062ec6ba89cc44d98d2423cba2ca06fc7755
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-16 11:56:24 -0300

    no eth client

--- sdk/tests/tests.rs
+++ sdk/tests/tests.rs
@@ -6,9 +6,9 @@ async fn sdk_integration_test() {
     assert!(true);
 }
 
-fn eth_client() -> EthClient {
-    EthClient::new(
-        &std::env::var("INTEGRATION_TEST_ETH_URL").unwrap_or(DEFAULT_ETH_URL.to_string()),
-    )
-    .unwrap()
-}
+// fn eth_client() -> EthClient {
+//     EthClient::new(
+//         &std::env::var("INTEGRATION_TEST_ETH_URL").unwrap_or(DEFAULT_ETH_URL.to_string()),
+//     )
+//     .unwrap()
+// }
