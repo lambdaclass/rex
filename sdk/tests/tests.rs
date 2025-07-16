dd2cd2752134ce46a124d4145e7e7691fa044915
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-16 11:56:09 -0300

    Run all

--- sdk/tests/tests.rs
+++ sdk/tests/tests.rs
@@ -1,4 +1,14 @@
+const DEFAULT_ETH_URL: &str = "http://localhost:8545";
+const DEFAULT_PROPOSER_URL: &str = "http://localhost:1729";
+
 #[tokio::test]
 async fn sdk_integration_test() {
     assert!(true);
+}
+
+fn eth_client() -> EthClient {
+    EthClient::new(
+        &std::env::var("INTEGRATION_TEST_ETH_URL").unwrap_or(DEFAULT_ETH_URL.to_string()),
+    )
+    .unwrap()
 }
