ae01df3c382a94a8552ea97894595a5670ef5ff5
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-16 14:34:02 -0300

    Use old commit

--- sdk/src/sdk.rs
+++ sdk/src/sdk.rs
@@ -3,6 +3,10 @@ use ethrex_common::types::GenericTransaction;
 use ethrex_common::{Address, H256, U256};
 use ethrex_rpc::types::receipt::RpcReceipt;
 use secp256k1::SecretKey;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::{Command, ExitStatus};
+use tracing::{info, trace};
 
 pub mod calldata;
 pub mod client;
@@ -112,6 +116,282 @@ pub fn balance_in_eth(eth: bool, balance: U256) -> String {
     } else {
         format!("{balance}")
     }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum GitError {
+    #[error("Failed to clone: {0}")]
+    DependencyError(String),
+    #[error("Internal error: {0}")]
+    InternalError(String),
+    #[error("Failed to get string from path")]
+    FailedToGetStringFromPath,
+}
+
+pub fn download_contract_deps(contracts_path: &Path) -> Result<(), GitError> {
+    trace!("Downloading contract dependencies");
+    std::fs::create_dir_all(contracts_path.join("lib")).map_err(|err| {
+        GitError::DependencyError(format!("Failed to create contracts/lib: {err}"))
+    })?;
+
+    git_clone(
+        "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable.git",
+        contracts_path
+            .join("lib/openzeppelin-contracts-upgradeable")
+            .to_str()
+            .ok_or(GitError::FailedToGetStringFromPath)?,
+        None,
+        true,
+    )?;
+
+    git_clone(
+        "https://github.com/succinctlabs/sp1-contracts.git",
+        contracts_path
+            .join("lib/sp1-contracts")
+            .to_str()
+            .ok_or(GitError::FailedToGetStringFromPath)?,
+        None,
+        false,
+    )?;
+
+    trace!("Contract dependencies downloaded");
+    Ok(())
+}
+
+pub fn git_clone(
+    repository_url: &str,
+    outdir: &str,
+    branch: Option<&str>,
+    submodules: bool,
+) -> Result<ExitStatus, GitError> {
+    info!(repository_url = %repository_url, outdir = %outdir, branch = ?branch, "Cloning or updating git repository");
+
+    if PathBuf::from(outdir).join(".git").exists() {
+        info!(outdir = %outdir, "Found existing git repository, updating...");
+
+        let branch_name = if let Some(b) = branch {
+            b.to_string()
+        } else {
+            // Look for default branch name (could be main, master or other)
+            let output = Command::new("git")
+                .current_dir(outdir)
+                .arg("symbolic-ref")
+                .arg("refs/remotes/origin/HEAD")
+                .output()
+                .map_err(|e| {
+                    GitError::DependencyError(format!(
+                        "Failed to get default branch for {outdir}: {e}"
+                    ))
+                })?;
+
+            if !output.status.success() {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                return Err(GitError::DependencyError(format!(
+                    "Failed to get default branch for {outdir}: {stderr}"
+                )));
+            }
+
+            String::from_utf8(output.stdout)
+                .map_err(|_| GitError::InternalError("Failed to parse git output".to_string()))?
+                .trim()
+                .split('/')
+                .next_back()
+                .ok_or(GitError::InternalError(
+                    "Failed to parse default branch".to_string(),
+                ))?
+                .to_string()
+        };
+
+        trace!(branch = %branch_name, "Updating to branch");
+
+        // Fetch
+        let fetch_status = Command::new("git")
+            .current_dir(outdir)
+            .args(["fetch", "origin"])
+            .spawn()
+            .map_err(|err| GitError::DependencyError(format!("Failed to spawn git fetch: {err}")))?
+            .wait()
+            .map_err(|err| {
+                GitError::DependencyError(format!("Failed to wait for git fetch: {err}"))
+            })?;
+        if !fetch_status.success() {
+            return Err(GitError::DependencyError(format!(
+                "git fetch failed for {outdir}"
+            )));
+        }
+
+        // Checkout to branch
+        let checkout_status = Command::new("git")
+            .current_dir(outdir)
+            .arg("checkout")
+            .arg(&branch_name)
+            .spawn()
+            .map_err(|err| {
+                GitError::DependencyError(format!("Failed to spawn git checkout: {err}"))
+            })?
+            .wait()
+            .map_err(|err| {
+                GitError::DependencyError(format!("Failed to wait for git checkout: {err}"))
+            })?;
+        if !checkout_status.success() {
+            return Err(GitError::DependencyError(format!(
+                "git checkout of branch {branch_name} failed for {outdir}, try deleting the repo folder"
+            )));
+        }
+
+        // Reset branch to origin
+        let reset_status = Command::new("git")
+            .current_dir(outdir)
+            .arg("reset")
+            .arg("--hard")
+            .arg(format!("origin/{branch_name}"))
+            .spawn()
+            .map_err(|err| GitError::DependencyError(format!("Failed to spawn git reset: {err}")))?
+            .wait()
+            .map_err(|err| {
+                GitError::DependencyError(format!("Failed to wait for git reset: {err}"))
+            })?;
+
+        if !reset_status.success() {
+            return Err(GitError::DependencyError(format!(
+                "git reset failed for {outdir}"
+            )));
+        }
+
+        // Update submodules
+        if submodules {
+            let submodule_status = Command::new("git")
+                .current_dir(outdir)
+                .arg("submodule")
+                .arg("update")
+                .arg("--init")
+                .arg("--recursive")
+                .spawn()
+                .map_err(|err| {
+                    GitError::DependencyError(format!(
+                        "Failed to spawn git submodule update: {err}"
+                    ))
+                })?
+                .wait()
+                .map_err(|err| {
+                    GitError::DependencyError(format!(
+                        "Failed to wait for git submodule update: {err}"
+                    ))
+                })?;
+            if !submodule_status.success() {
+                return Err(GitError::DependencyError(format!(
+                    "git submodule update failed for {outdir}"
+                )));
+            }
+        }
+
+        Ok(reset_status)
+    } else {
+        trace!(repository_url = %repository_url, outdir = %outdir, branch = ?branch, "Cloning git repository");
+        let mut git_cmd = Command::new("git");
+
+        let git_clone_cmd = git_cmd.arg("clone").arg(repository_url);
+
+        if let Some(branch) = branch {
+            git_clone_cmd.arg("--branch").arg(branch);
+        }
+
+        if submodules {
+            git_clone_cmd.arg("--recurse-submodules");
+        }
+
+        git_clone_cmd
+            .arg(outdir)
+            .spawn()
+            .map_err(|err| GitError::DependencyError(format!("Failed to spawn git: {err}")))?
+            .wait()
+            .map_err(|err| GitError::DependencyError(format!("Failed to wait for git: {err}")))
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ContractCompilationError {
+    #[error("The path is not a valid utf-8 string")]
+    FailedToGetStringFromPath,
+    #[error("Deployer compilation error: {0}")]
+    CompilationError(String),
+    #[error("Could not read file")]
+    FailedToReadFile(#[from] std::io::Error),
+    #[error("Failed to serialize/deserialize")]
+    SerializationError(#[from] serde_json::Error),
+}
+
+pub fn compile_contract(
+    general_contracts_path: &Path,
+    contract_path: &str,
+    runtime_bin: bool,
+) -> Result<(), ContractCompilationError> {
+    let bin_flag = if runtime_bin {
+        "--bin-runtime"
+    } else {
+        "--bin"
+    };
+
+    // Both the contract path and the output path are relative to where the Makefile is.
+    if !Command::new("solc")
+        .arg(bin_flag)
+        .arg(
+            "@openzeppelin/contracts=".to_string()
+                + general_contracts_path
+                    .join("lib")
+                    .join("openzeppelin-contracts-upgradeable")
+                    .join("lib")
+                    .join("openzeppelin-contracts")
+                    .join("contracts")
+                    .to_str()
+                    .ok_or(ContractCompilationError::FailedToGetStringFromPath)?,
+        )
+        .arg(
+            "@openzeppelin/contracts-upgradeable=".to_string()
+                + general_contracts_path
+                    .join("lib")
+                    .join("openzeppelin-contracts-upgradeable")
+                    .join("contracts")
+                    .to_str()
+                    .ok_or(ContractCompilationError::FailedToGetStringFromPath)?,
+        )
+        .arg(
+            general_contracts_path
+                .join(contract_path)
+                .to_str()
+                .ok_or(ContractCompilationError::FailedToGetStringFromPath)?,
+        )
+        .arg("--via-ir")
+        .arg("-o")
+        .arg(
+            general_contracts_path
+                .join("solc_out")
+                .to_str()
+                .ok_or(ContractCompilationError::FailedToGetStringFromPath)?,
+        )
+        .arg("--overwrite")
+        .arg("--allow-paths")
+        .arg(
+            general_contracts_path
+                .to_str()
+                .ok_or(ContractCompilationError::FailedToGetStringFromPath)?,
+        )
+        .spawn()
+        .map_err(|err| {
+            ContractCompilationError::CompilationError(format!("Failed to spawn solc: {err}"))
+        })?
+        .wait()
+        .map_err(|err| {
+            ContractCompilationError::CompilationError(format!("Failed to wait for solc: {err}"))
+        })?
+        .success()
+    {
+        return Err(ContractCompilationError::CompilationError(
+            format!("Failed to compile {contract_path}").to_owned(),
+        ));
+    }
+
+    Ok(())
 }
 
 #[test]
--- sdk/tests/tests.rs
+++ sdk/tests/tests.rs
@@ -1,6 +1,13 @@
-use ethrex_common::{Address, H160, H256};
+use ethrex_common::{Address, Bytes, H160, H256, U256, types::BlockNumber};
+use ethrex_l2::sequencer::l1_watcher::PrivilegedTransactionData;
+use ethrex_rpc::types::receipt::RpcReceipt;
+use keccak_hash::keccak;
 use rex_sdk::client::EthClient;
+use rex_sdk::client::Overrides;
+use rex_sdk::client::eth::BlockByNumber;
 use rex_sdk::client::eth::get_address_from_secret_key;
+use rex_sdk::l2::deposit::deposit_through_transfer;
+use rex_sdk::wait_for_transaction_receipt;
 use secp256k1::SecretKey;
 use std::{
     fs::File,
@@ -26,18 +33,34 @@ const DEFAULT_BRIDGE_ADDRESS: Address = H160([
     0x8c, 0xcf, 0x74, 0x99, 0x9c, 0x49, 0x6e, 0x4d, 0x27, 0xa2, 0xb0, 0x29, 0x41, 0x67, 0x3f, 0x41,
     0xdd, 0x0d, 0xab, 0x2a,
 ]);
+// 0x0007a881CD95B1484fca47615B64803dad620C8d
+const DEFAULT_PROPOSER_COINBASE_ADDRESS: Address = H160([
+    0x00, 0x07, 0xa8, 0x81, 0xcd, 0x95, 0xb1, 0x48, 0x4f, 0xca, 0x47, 0x61, 0x5b, 0x64, 0x80, 0x3d,
+    0xad, 0x62, 0x0c, 0x8d,
+]);
 
 #[tokio::test]
-async fn sdk_integration_test() {
+async fn sdk_integration_test() -> Result<(), Box<dyn std::error::Error>> {
     read_env_file_by_config();
 
     let eth_client = eth_client();
     let proposer_client = proposer_client();
     let rich_wallet_private_key = l1_rich_wallet_private_key();
-    let transfer_return_private_key = l2_return_transfer_private_key();
+    let _transfer_return_private_key = l2_return_transfer_private_key();
     let bridge_address = common_bridge_address();
     let deposit_recipient_address = get_address_from_secret_key(&rich_wallet_private_key)
         .expect("Failed to get address from l1 rich wallet pk");
+
+    test_deposit(
+        &rich_wallet_private_key,
+        bridge_address,
+        deposit_recipient_address,
+        &eth_client,
+        &proposer_client,
+    )
+    .await?;
+
+    Ok(())
 }
 
 pub fn read_env_file_by_config() {
@@ -103,4 +126,161 @@ fn common_bridge_address() -> Address {
             );
             DEFAULT_BRIDGE_ADDRESS
         })
+}
+
+fn fees_vault() -> Address {
+    std::env::var("INTEGRATION_TEST_PROPOSER_COINBASE_ADDRESS")
+        .map(|address| address.parse().expect("Invalid proposer coinbase address"))
+        .unwrap_or(DEFAULT_PROPOSER_COINBASE_ADDRESS)
+}
+
+async fn test_deposit(
+    depositor_private_key: &SecretKey,
+    bridge_address: Address,
+    deposit_recipient_address: Address,
+    eth_client: &EthClient,
+    proposer_client: &EthClient,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Fetching initial balances on L1 and L2");
+
+    let depositor = get_address_from_secret_key(depositor_private_key)?;
+    let deposit_value = std::env::var("INTEGRATION_TEST_DEPOSIT_VALUE")
+        .map(|value| U256::from_dec_str(&value).expect("Invalid deposit value"))
+        .unwrap_or(U256::from(1000000000000000000000u128));
+
+    let depositor_l1_initial_balance = eth_client
+        .get_balance(depositor, BlockByNumber::Latest)
+        .await?;
+
+    assert!(
+        depositor_l1_initial_balance >= deposit_value,
+        "L1 depositor doesn't have enough balance to deposit"
+    );
+
+    let deposit_recipient_l2_initial_balance = proposer_client
+        .get_balance(deposit_recipient_address, BlockByNumber::Latest)
+        .await?;
+
+    let bridge_initial_balance = eth_client
+        .get_balance(bridge_address, BlockByNumber::Latest)
+        .await?;
+
+    let fee_vault_balance_before_deposit = proposer_client
+        .get_balance(fees_vault(), BlockByNumber::Latest)
+        .await?;
+
+    println!("Depositing funds from L1 to L2");
+
+    let deposit_tx_hash = deposit_through_transfer(
+        deposit_value,
+        deposit_recipient_address,
+        depositor_private_key,
+        bridge_address,
+        eth_client,
+    )
+    .await?;
+
+    println!("Waiting for L1 deposit transaction receipt");
+
+    let deposit_tx_receipt =
+        wait_for_transaction_receipt(deposit_tx_hash, eth_client, 5, true).await?;
+
+    let depositor_l1_balance_after_deposit = eth_client
+        .get_balance(depositor, BlockByNumber::Latest)
+        .await?;
+
+    assert_eq!(
+        depositor_l1_balance_after_deposit,
+        depositor_l1_initial_balance
+            - deposit_value
+            - deposit_tx_receipt.tx_info.gas_used * deposit_tx_receipt.tx_info.effective_gas_price,
+        "Depositor L1 balance didn't decrease as expected after deposit"
+    );
+
+    let bridge_balance_after_deposit = eth_client
+        .get_balance(bridge_address, BlockByNumber::Latest)
+        .await?;
+
+    assert_eq!(
+        bridge_balance_after_deposit,
+        bridge_initial_balance + deposit_value,
+        "Bridge balance didn't increase as expected after deposit"
+    );
+
+    println!("Waiting for L2 deposit tx receipt");
+
+    let _ = wait_for_l2_deposit_receipt(
+        deposit_tx_receipt.block_info.block_number,
+        eth_client,
+        proposer_client,
+    )
+    .await?;
+
+    let deposit_recipient_l2_balance_after_deposit = proposer_client
+        .get_balance(deposit_recipient_address, BlockByNumber::Latest)
+        .await?;
+
+    assert_eq!(
+        deposit_recipient_l2_balance_after_deposit,
+        deposit_recipient_l2_initial_balance + deposit_value,
+        "Deposit recipient L2 balance didn't increase as expected after deposit"
+    );
+
+    let fee_vault_balance_after_deposit = proposer_client
+        .get_balance(fees_vault(), BlockByNumber::Latest)
+        .await?;
+
+    assert_eq!(
+        fee_vault_balance_after_deposit, fee_vault_balance_before_deposit,
+        "Fee vault balance should not change after deposit"
+    );
+
+    Ok(())
+}
+
+async fn wait_for_l2_deposit_receipt(
+    l1_receipt_block_number: BlockNumber,
+    eth_client: &EthClient,
+    proposer_client: &EthClient,
+) -> Result<RpcReceipt, Box<dyn std::error::Error>> {
+    let topic = keccak(b"PrivilegedTxSent(address,address,address,uint256,uint256,uint256,bytes)");
+    let logs = eth_client
+        .get_logs(
+            U256::from(l1_receipt_block_number),
+            U256::from(l1_receipt_block_number),
+            common_bridge_address(),
+            topic,
+        )
+        .await?;
+    let data = PrivilegedTransactionData::from_log(logs.first().unwrap().log.clone())?;
+
+    let l2_deposit_tx_hash = eth_client
+        .build_privileged_transaction(
+            data.to_address,
+            data.from,
+            Bytes::copy_from_slice(&data.calldata),
+            Overrides {
+                chain_id: Some(proposer_client.get_chain_id().await?.try_into().unwrap()),
+                // Using the transaction_id as nonce.
+                // If we make a transaction on the L2 with this address, we may break the
+                // privileged transaction workflow.
+                nonce: Some(data.transaction_id.as_u64()),
+                value: Some(data.value),
+                gas_limit: Some(data.gas_limit.as_u64()),
+                // TODO(CHECK): Seems that when we start the L2, we need to set the gas.
+                // Otherwise, the transaction is not included in the mempool.
+                // We should override the blockchain to always include the transaction.
+                max_fee_per_gas: Some(0),
+                max_priority_fee_per_gas: Some(0),
+                ..Default::default()
+            },
+        )
+        .await
+        .unwrap()
+        .get_privileged_hash()
+        .unwrap();
+
+    println!("Waiting for deposit transaction receipt on L2");
+
+    Ok(wait_for_transaction_receipt(l2_deposit_tx_hash, proposer_client, 1000, true).await?)
 }
