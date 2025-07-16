25c7ac52924245852c4acbb14b507d6de7a28c36
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-16 11:57:29 -0300

    Add client

--- sdk/tests/tests.rs
+++ sdk/tests/tests.rs
@@ -1,3 +1,5 @@
+use rex_sdk::client::EthClient;
+
 const DEFAULT_ETH_URL: &str = "http://localhost:8545";
 const DEFAULT_PROPOSER_URL: &str = "http://localhost:1729";
 
@@ -6,9 +8,9 @@ async fn sdk_integration_test() {
     assert!(true);
 }
 
-// fn eth_client() -> EthClient {
-//     EthClient::new(
-//         &std::env::var("INTEGRATION_TEST_ETH_URL").unwrap_or(DEFAULT_ETH_URL.to_string()),
-//     )
-//     .unwrap()
-// }
+fn eth_client() -> EthClient {
+    EthClient::new(
+        &std::env::var("INTEGRATION_TEST_ETH_URL").unwrap_or(DEFAULT_ETH_URL.to_string()),
+    )
+    .unwrap()
+}
