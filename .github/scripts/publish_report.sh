780143d7d1d1804813bb6d56469fd5e0bb1f977f
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2025-08-22 19:23:49 -0300

    chore(ci): update daily check (#183)
    **Motivation**
    
    The daily check was being sent to a test channel instead of the internal
    ethrex L2 slack channel and the slack msg header and content were not
    the best

--- .github/scripts/publish_report.sh
+++ .github/scripts/publish_report.sh
@@ -7,7 +7,7 @@ $(jq -n --arg text "$2" '{
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": "Rex report"
+                "text": "Rex Daily Check"
             }
         },
         {
